a617ae0dcb0696c80f98c06e774df97aa0b74b06
Orsen6 <orsenkul@gmail.com>
2021-07-29 17:26:03 +0300

    added left sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
 
+    $(".shop-filters_price-input").ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            $('.shop-filters_price-num--start').text(100);
+            $('.shop-filters_price-num--finish').text(500);
+        },
+        onChange: function (data) {
+            $('.shop-filters_price-num--start').text(data.from);
+            $('.shop-filters_price-num--finish').text(data.to);
+        },
+        
+    });
+
+    $(function () {
+ 
+        $("#recent-star, #recent-star2, #recent-star3").rateYo({
+          starWidth: "12px",
+          rating    : 4,
+          spacing: "5px",
+          readOnly: true
+        });
+       
+      });
+
     $('.slider-inner').slick({
 
         arrows: false,
